16c010bc1bb2a7caeb9064ac66a7f3cf60d6c158
Juan Camilo Bazurto <juan.bazurto@mail.escuelaing.edu.co>
2023-02-28 12:02:46 -0500

    reto_1_react

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js", "tsx"],
+  images: {
+    domains: ["raw.githubusercontent.com"],
+    minimumCacheTTL: 90000,
+    formats: ["image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
